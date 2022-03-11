8e1b04f7ea1e9afb1d63283c187cb92648c271cd
jiawei_yao <jsntyjw@gmail.com>
2022-03-11 20:03:04 +0800

    update

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -25,76 +25,79 @@ class Home extends React.Component<IProps, IState> {
   }
 
   public render(): React.ReactNode {
-    var sessionResult = sessionStorage.getItem('data') || '{}'
-    var obj = JSON.parse(sessionResult);
-    console.log(sessionResult)
-    
-    var role = obj.Role
-    if(role == 'admin'){
-      return (
+    // var sessionResult = sessionStorage.getItem('data') || '{}'
+    // var obj = JSON.parse(sessionResult);
+    // console.log(sessionResult)
 
-        <div>
-  
-  
-          <br></br> <br></br> 
-          <MasonryTileGridContainer numOfCols={3}>
-  
-            <MasonryTile colEnd={1} colStart={1}>
-              <ImageSelector
-                href={process.env.REACT_APP_MY_EC2_ADDRESS + "Appointments"}
-                description="View Appointments!" imgSrc="https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/12%20Best%20Appointment%20Booking%20Apps%20HERO1627253223960628.jpg" title="Appointments" />
-            </MasonryTile>
-            <MasonryTile colEnd={2} colStart={2}>
-              <ImageSelector
-                href={process.env.REACT_APP_MY_EC2_ADDRESS + "Dashboard"}
-                description="View Queue Dashboard" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgOT-CTWskmfFzeOF9Ice8xI03VpDoVgnUpyfoYoYrg6VqGJ4W_7tH9CrzFT8-jqOglig&usqp=CAU" title="Dashboard" />
-            </MasonryTile>
-  
-            <MasonryTile colEnd={3} colStart={3}>
-              <ImageSelector
-                href={process.env.REACT_APP_MY_EC2_ADDRESS + "ServiceCounter"}
-  
-                description="Service Counter Page." imgSrc="http://bookinjiffy.com/wp-content/uploads/2020/08/calandar-digital-laptop-phone-760.jpg" title="Service Counter" />
-            </MasonryTile>
-  
-          </MasonryTileGridContainer>
-  
-        </div>
-      );
-    }
-    if(role == 'citizen'){
-      return (
+    // var role = obj.Role
+    // if(role == 'admin'){
+    return (
 
-        <div>
-  
-  
-          <br></br> <br></br> 
-          <MasonryTileGridContainer numOfCols={3}>
-  
-            <MasonryTile colEnd={1} colStart={1}>
-              <ImageSelector
-                href={process.env.REACT_APP_MY_EC2_ADDRESS + "Create"}
-                description="Create a new booking here!" imgSrc="https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/12%20Best%20Appointment%20Booking%20Apps%20HERO1627253223960628.jpg" title="Create Appointments" />
-            </MasonryTile>
-            <MasonryTile colEnd={2} colStart={2}>
-              <ImageSelector
-                href={process.env.REACT_APP_MY_EC2_ADDRESS + "Dashboard"}
-                description="View Queue Dashboard" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgOT-CTWskmfFzeOF9Ice8xI03VpDoVgnUpyfoYoYrg6VqGJ4W_7tH9CrzFT8-jqOglig&usqp=CAU" title="Dashboard" />
-            </MasonryTile>
-  
-            <MasonryTile colEnd={3} colStart={3}>
-              <ImageSelector
-                href={process.env.REACT_APP_MY_EC2_ADDRESS + "ViewMyAppointment"}
-  
-                description="Click here to view all my bookings." imgSrc="http://bookinjiffy.com/wp-content/uploads/2020/08/calandar-digital-laptop-phone-760.jpg" title="View My Appointments" />
-            </MasonryTile>
-  
-          </MasonryTileGridContainer>
-  
-        </div>
-      );
-    }
-    
+      <div>
+
+
+        <br></br> <br></br>
+        <MasonryTileGridContainer numOfCols={3}>
+
+          <MasonryTile colEnd={1} colStart={1}>
+            <ImageSelector
+              href={process.env.REACT_APP_MY_EC2_ADDRESS + "Appointments"}
+              description="View Appointments!" imgSrc="https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/12%20Best%20Appointment%20Booking%20Apps%20HERO1627253223960628.jpg" title="Appointments" />
+          </MasonryTile>
+          <MasonryTile colEnd={2} colStart={2}>
+            <ImageSelector
+              href={process.env.REACT_APP_MY_EC2_ADDRESS + "Dashboard"}
+              description="View Queue Dashboard" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgOT-CTWskmfFzeOF9Ice8xI03VpDoVgnUpyfoYoYrg6VqGJ4W_7tH9CrzFT8-jqOglig&usqp=CAU" title="Dashboard" />
+          </MasonryTile>
+
+          <MasonryTile colEnd={3} colStart={3}>
+            <ImageSelector
+              href={process.env.REACT_APP_MY_EC2_ADDRESS + "ServiceCounter"}
+
+              description="Service Counter Page." imgSrc="http://bookinjiffy.com/wp-content/uploads/2020/08/calandar-digital-laptop-phone-760.jpg" title="Service Counter" />
+          </MasonryTile>
+
+        </MasonryTileGridContainer>
+
+      </div>
+    );
   }
+
 }
+    // if(role == 'citizen'){
+      // return (
+
+    //     <div>
+
+
+    //       <br></br> <br></br> 
+    //       <MasonryTileGridContainer numOfCols={3}>
+
+    //         <MasonryTile colEnd={1} colStart={1}>
+    //           <ImageSelector
+    //             href={process.env.REACT_APP_MY_EC2_ADDRESS + "Create"}
+    //             description="Create a new booking here!" imgSrc="https://www.hp.com/us-en/shop/app/assets/images/uploads/prod/12%20Best%20Appointment%20Booking%20Apps%20HERO1627253223960628.jpg" title="Create Appointments" />
+    //         </MasonryTile>
+    //         <MasonryTile colEnd={2} colStart={2}>
+    //           <ImageSelector
+    //             href={process.env.REACT_APP_MY_EC2_ADDRESS + "Dashboard"}
+    //             description="View Queue Dashboard" imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgOT-CTWskmfFzeOF9Ice8xI03VpDoVgnUpyfoYoYrg6VqGJ4W_7tH9CrzFT8-jqOglig&usqp=CAU" title="Dashboard" />
+    //         </MasonryTile>
+
+    //         <MasonryTile colEnd={3} colStart={3}>
+    //           <ImageSelector
+    //             href={process.env.REACT_APP_MY_EC2_ADDRESS + "ViewMyAppointment"}
+
+    //             description="Click here to view all my bookings." imgSrc="http://bookinjiffy.com/wp-content/uploads/2020/08/calandar-digital-laptop-phone-760.jpg" title="View My Appointments" />
+    //         </MasonryTile>
+
+    //       </MasonryTileGridContainer>
+
+    //     </div>
+    //   );
+    // }
+
+  // }
+// }
+
 export default Home;
